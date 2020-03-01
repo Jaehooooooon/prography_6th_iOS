132ddfb08200d829b17d959d88ed67293cf5abd7
Jaehooooooon <jaehoon572@naver.com>
2020-03-01 16:02:07 +0900

    Initialize

---
+++ Prography_6th_iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prography_6th_iOS
+//
+//  Created by 서재훈 on 2020/03/01.
+//  Copyright © 2020 서재훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
